a2c7a8dccff5d47f919b4c23f2a2a95af363a0c1
yvsdarahas <yvsdarahas@gmail.com>
2024-03-16 15:58:01 +0100

    Improve code quality
    Removed white spaces and improved in indentation.

--- clientmain.cpp
+++ clientmain.cpp
@@ -21,43 +21,43 @@
 
 int main(int argc, char *argv[]){
 
-  /*
-    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
-     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
-  */
-  char delim[]=":";
-  char *Desthost=strtok(argv[1],delim);
-  char *Destport=strtok(NULL,delim);
-  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-  // *Dstport points to whatever string came after the delimiter. 
+	/*
+		Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port).
+		Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'.
+	*/
+	char delim[]=":";
+	char *Desthost=strtok(argv[1],delim);
+	char *Destport=strtok(NULL,delim);
+	// *Desthost now points to a sting holding whatever came before the delimiter, ':'.
+	// *Dstport points to whatever string came after the delimiter.
 
-  /* Do magic */
-  int port=atoi(Destport);
-  #ifdef DEBUG 
-    printf("Host %s, and port %d.\n",Desthost,port);
-  #endif
+	/* Do magic */
+	int port=atoi(Destport);
+	#ifdef DEBUG
+		printf("Host %s, and port %d.\n",Desthost,port);
+	#endif
 
-  //Getting the address info(Ipv4/Ipv6/Dns)
-  //Useful when dns host is used
-  struct addrinfo hints, *res;
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
+	//Getting the address info(Ipv4/Ipv6/Dns)
+	//Useful when dns host is used
+	struct addrinfo hints, *res;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 
-  int addr_info = getaddrinfo(Desthost, Destport, &hints, &res);
-  if (addr_info != 0){
-    printf("\n Error in getting Server's info \n");
-    exit(1);
-  }
+	int addr_info = getaddrinfo(Desthost, Destport, &hints, &res);
+	if (addr_info != 0){
+		printf("\n Error in getting Server's info \n");
+		exit(1);
+	}
 
 	//Initialising Socket
 	int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 	if (sockfd < 0){
-	  printf("\n Error creating socket \n");
-	  exit(1);
+		printf("\n Error creating socket \n");
+		exit(1);
 	}
 
-  struct sockaddr_in server_addr;
+	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(port);
 	server_addr.sin_addr.s_addr = inet_addr(Desthost);
@@ -65,8 +65,8 @@ int main(int argc, char *argv[]){
 	//Connection to the server is established here
 	int connection = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 	if (connection < 0){
-	  perror("\n Error in connection");
-	  exit(1);
+		perror("\n Error in connection");
+		exit(1);
 	}
 
 	//If server successfully accepts the connection then it sends its version as "TEXT TCP 1.0"
@@ -74,8 +74,8 @@ int main(int argc, char *argv[]){
 	char from_server[256], to_server[256];
 	int recv_len = recv(sockfd, &from_server, sizeof(from_server), 0);
 	if (recv_len < 0) {
-	  printf("\n Error recv\n");
-	  exit(1);
+		printf("\n Error recv\n");
+		exit(1);
 	}
 
 	//If the clients accepts this protocol then it sends message "OK" or it sends message "Error"
@@ -83,24 +83,24 @@ int main(int argc, char *argv[]){
 	int rc = regcomp(&regex, "TCP 1.0", 0);
 	rc = regexec(&regex, from_server, 0, NULL, 0);
 	if (rc != 0) {
-	  printf("\n No supported protocol \n");
-	  exit(1);
+		printf("\n No supported protocol \n");
+		exit(1);
 	}
 	printf("\n %s", from_server);
 	memset(to_server, 0, sizeof(to_server));
 	strcat(to_server, "OK\n");
 	int send_len = send(sockfd, to_server, strlen(to_server), 0);
 	if (send_len < 0) {
-	  printf("\n Error send\n");
-	  exit(1);
+		printf("\n Error send\n");
+		exit(1);
 	}
 
 	//Error handling of received buffer from server
 	memset(from_server, 0, sizeof(from_server));
 	recv_len = recv(sockfd, &from_server, sizeof(from_server), 0);
 	if (recv_len < 0) {
-	  printf("\n Error recv\n");
-	  exit(1);
+		printf("\n Error recv\n");
+		exit(1);
 	}
 
 	//Here server sends the 2 values and its operation to do <value1> <value2> <operation>
@@ -123,55 +123,55 @@ int main(int argc, char *argv[]){
 
 	//If it is integer operation add, sub, mul, div
 	if (oper[0][0] != 'f') {
-	  if (strcmp("add", oper[0]) == 0) {
-	    result = o1 + o2;
-	  }
-	  else if (strcmp("sub", oper[0]) == 0) {
-	    result = o1 - o2;
-	  }
-	  else if (strcmp("mul", oper[0]) == 0) {
-	    result = o1 * o2;
-	  }
-	  else if (strcmp("div", oper[0]) == 0) {
-	    result = o1 / o2;
-	  }
-	  memset(to_server, 0, sizeof(to_server));
-	  sprintf(to_server, "%d\n", result);
-	  printf("\n Result : %d \n", result);
+		if (strcmp("add", oper[0]) == 0) {
+			result = o1 + o2;
+		}
+		else if (strcmp("sub", oper[0]) == 0) {
+			result = o1 - o2;
+		}
+		else if (strcmp("mul", oper[0]) == 0) {
+			result = o1 * o2;
+		}
+		else if (strcmp("div", oper[0]) == 0) {
+			result = o1 / o2;
+		}
+		memset(to_server, 0, sizeof(to_server));
+		sprintf(to_server, "%d\n", result);
+		printf("\n Result : %d \n", result);
 	}
 
 	//If it is float operation fadd, fsub, fmul, fdiv
 	else {
-	  if (strcmp("fadd", oper[0]) == 0) {
-	    fresult = f1 + f2;
-	  }
-	  else if (strcmp("fsub", oper[0]) == 0) {
-	    fresult = f1 - f2;
-	  }
-	  else if (strcmp("fmul", oper[0]) == 0) {
-	    fresult = f1 * f2;
-	  }
-	  else if (strcmp("fdiv", oper[0]) == 0) {
-	    fresult = f1 / f2;
-	  }
-	  memset(to_server, 0, sizeof(to_server));
-	  sprintf(to_server, "%8.8g\n", fresult);
-	  printf("\n Result : %8.8g \n", fresult);
+		if (strcmp("fadd", oper[0]) == 0) {
+			fresult = f1 + f2;
+		}
+		else if (strcmp("fsub", oper[0]) == 0) {
+			fresult = f1 - f2;
+		}
+		else if (strcmp("fmul", oper[0]) == 0) {
+			fresult = f1 * f2;
+		}
+		else if (strcmp("fdiv", oper[0]) == 0) {
+			fresult = f1 / f2;
+		}
+		memset(to_server, 0, sizeof(to_server));
+		sprintf(to_server, "%8.8g\n", fresult);
+		printf("\n Result : %8.8g \n", fresult);
 	}
 
 	//Send the result to the server in the form "result\n"
 	send_len = send(sockfd, to_server, strlen(to_server), 0);
 	if (send_len < 0) {
-	  printf("\n Error send\n");
-	  exit(1);
+		printf("\n Error send\n");
+		exit(1);
 	}
 
 	//Receive message "OK" if the result is correct or message "Error" if result is wrong
 	memset(from_server, 0, sizeof(from_server));
 	recv_len = recv(sockfd, &from_server, sizeof(from_server), 0);
 	if (recv_len < 0) {
-	  printf("\n Error recv\n");
-	  exit(1);
+		printf("\n Error recv\n");
+		exit(1);
 	}
 	printf("\n Server response : %s", from_server);
 	close(sockfd);
